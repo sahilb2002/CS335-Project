b9e67088144804348b94972eb640a59c92b22de8
Parth Maniar <pmaniar20@iitk.ac.in>
2023-04-03 19:00:29 +0530

    static keyword resolve

--- tests/t.java
+++ tests/t.java
@@ -1,9 +1,9 @@
 public class PM_Chutiya {
     static int k = 0;
-    public void main(String args) {
+    public static void main(String args) {
         int i=0;
         String s = args;
-       int a = k;
+       //int a = k;
         for(i=0; i<10; i++) {
             System.out.println("Hello World");
         }
