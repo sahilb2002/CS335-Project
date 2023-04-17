96f83d0bd4a6802a997f29f05f11fdcaebeda1e5
sahil bansal <sahilb20@iitk.ac.in>
2023-04-17 18:08:22 +0530

    added freereg

--- include/codegen.h
+++ include/codegen.h
@@ -9,7 +9,9 @@
 
 using namespace std;
 
-#define RAX
+
+#define RSP "%rsp"
+#define RBP "%rbp"
 
 #define RAX "rax"
 #define RBX "rbx"
@@ -20,23 +22,26 @@ using namespace std;
 #define R8 "r8"
 #define R9 "r9"
 #define R10 "r10"
-#define R12 "r11"
-#define R13"r12"
-#define R14 "r13"
-#define R15 "r14"
-#define R16 "r15"
+#define R11 "r11"
+#define R12"r12"
+#define R13 "r13"
+#define R14 "r14"
+#define R15 "r15"
+
+#define NO_FREE_REG ""
 
 typedef string reg;
 typedef pair<string, SymbTbl_entry*> var;
 typedef vector<var> reg_desc;
-map<reg, reg_desc> reg_map;
 
 void basicBlock();
-void regAlloc();
 
 void init_reg_map();
 reg get_free_reg();
-void free_reg(reg r);
+
+// free_reg frees the register r
+// returns 0 on success, -1 if no such reg r
+int free_reg(reg r);
 reg get_reg(quad instr);
 
 #endif
--- src/codegen.cpp
+++ src/codegen.cpp
@@ -2,22 +2,73 @@
 
 extern vector<quad>code;
 
+typedef vector<string> x86_instr;
+vector<x86_instr> x86_code;
+
 vector<int>leaders;
 void basicBlock(){
     for(int i=0; i<code.size(); i++){
         if(i==0){
             leaders.push_back(i);
         }
-        else if(code[i].op == GOTO){
+        else if(code[i].op.first == GOTO){
             leaders.push_back(code[i].idx);
             leaders.push_back(i+1);
         }
-        else if(code[i].op == IFFALSE){
+        else if(code[i].op.first == IFFALSE){
             leaders.push_back(code[i].idx);
             leaders.push_back(i+1);
         }
-        else if(code[i].op == "func"){
+        else if(code[i].op.first == "func"){
             leaders.push_back(i);
+        }
+    }
+}
+
+map<reg, reg_desc> reg_map;
+
+void init_reg_map(){
+    reg_map.clear();
+    reg_map.insert({RAX, {}});
+    reg_map.insert({RBX, {}});
+    reg_map.insert({RCX, {}});
+    reg_map.insert({RDX, {}});
+    reg_map.insert({RSI, {}});
+    reg_map.insert({RDI, {}});
+    reg_map.insert({R8, {}});
+    reg_map.insert({R9, {}});
+    reg_map.insert({R10, {}});
+    reg_map.insert({R12, {}});
+    reg_map.insert({R13, {}});
+    reg_map.insert({R14, {}});
+    reg_map.insert({R15, {}});
+}
+
+reg get_free_reg(){
+    for(auto it :reg_map){
+        if(it.second.size() == 0)
+            return it.first;
+    }
+    return NO_FREE_REG;
+}
+
+int free_reg(reg r){
+    auto it = reg_map.find(r);
+    if(it==reg_map.end())
+    return -1;
+
+    for(auto vars: it->second){
+        if(!vars.second){
+            // symbltbl entry is null
+            continue;
+        }
+        if(!vars.second->addr_desc.in_mem){
+            // var is not in mem so need to push it to its location in stack.
+
+            // assumes all vars are in stack.
+            int location = -vars.second->offset;
+            x86_instr
+
         }
     }
 }
